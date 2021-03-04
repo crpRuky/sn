4a3d6ca0674683a425fcdaa02187f625195f6e22
Me <denvoloshko@gmail.com>
2021-03-04 15:10:27 +0300

    main page changed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,34 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-container>
+    <v-row class="text-center">
+      <v-col cols="12">
+        <v-img
+          :src="require('../assets/logo.svg')"
+          class="my-3"
+          contain
+          height="200"
+        />
+      </v-col>
+
+      <v-col class="mb-4">
+        <h1 class="display-2 font-weight-bold mb-3">
+          Это не вконтакте
+        </h1>
+
+        <p class="subheading font-weight-regular">
+          Пожалуйста, перестанте писать что это вконтакте,
+          <br>вконтакте лучше.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
   }
 }
 </script>
